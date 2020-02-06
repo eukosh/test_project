f498e49812f65c936d2dc6d7b439a85354d69f92
eukosh <eugene.kosharnyi@gmail.com>
2020-02-06 12:45:07 +0200

    Added 'voting' functionality. Modified with, changed them to generic views.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,32 +1,39 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.views import generic
+from django.db.models import F
 
-from .models import Question
-
-
-def index(request):
-    questions = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'questions_list': questions,
-    }
-    return render(request, 'polls/index.html', context)
+from .models import Question, Choice
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    choices = question.choice_set.all()
-    context = {
-        'question': question,
-        'choices': choices,
-    }
-    return render(request, 'polls/detail.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'questions_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    response = f'You are looking at the results of question {question_id}.'
-    return HttpResponse(response)
+class DetailView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-    response = f'You are voting on question {question_id}.'
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return redirect('polls:results', pk=question.id)
